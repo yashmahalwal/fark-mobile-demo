4dc41c36d6bbf3eafe93902731fb3bb355cb8cc8
Yash Mahalwal <yashmahalwal@gmail.com>
2026-01-06 17:03:51 +0530

    chore: ignore .idea

---
+++ app/build/generated/source/buildConfig/debug/com/fark/mobiledemo/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fark.mobiledemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fark.mobiledemo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String REST_API_BASE_URL = "http://10.0.2.2:3000/api/";
+}
